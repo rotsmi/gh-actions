f5870a2e7f9e5edd7ef8bf28ab708133b53b80db
Alex Garcia <rotsmi@gmail.com>
2022-01-05 14:40:49 -0500

    fix: add more test (#21)

--- sub-repo1/test_utils.py
+++ sub-repo1/test_utils.py
@@ -6,5 +6,8 @@ def test_suma():
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
 
+def test_capital_capital_case():
+    assert capital_case('Semaphore') == 'Semaphore'
+
 def test_failed_capital_case():
     assert capital_case('semaphore') != 'semaphore'
