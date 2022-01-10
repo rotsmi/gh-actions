83986b9b0b769881b867374caa9d3fac55c9202b
rotsmi <rotsmi@gmail.com>
2022-01-10 15:56:12 -0500

    feat: add more teet

--- sub-repo1/test_utils.py
+++ sub-repo1/test_utils.py
@@ -9,5 +9,8 @@ def test_capital_case():
 def test_capital_capital_case():
     assert capital_case('Semaphore') == 'Semaphore'
 
+def test_capital_case_v2():
+    assert capital_case('Semaphore') == 'Semaphore'
+
 def test_failed_capital_case():
     assert capital_case('semaphore') != 'semaphore'
