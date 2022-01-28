ace071f1e9247cb0eb150a649a805ccfea71041c
rotsmi <rotsmi@gmail.com>
2022-01-28 07:37:50 -0500

    sb repo1

--- sub-repo1/test_utils.py
+++ sub-repo1/test_utils.py
@@ -17,3 +17,6 @@ def test_failed_capital_case():
 
 def test_success_capital_case():
     assert capital_case('capital') == 'Capital'
+
+def test_success_capital_case_one():
+    assert capital_case('one') == 'One'
